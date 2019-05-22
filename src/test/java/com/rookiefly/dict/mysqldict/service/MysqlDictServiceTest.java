d4f697775373324b5b740432a7e446fdc786d8c4
rookiefly <rookiefly@163.com>
2019-05-23 00:10:04 +0800

    add unit tests

---
+++ src/test/java/com/rookiefly/dict/mysqldict/service/MysqlDictServiceTest.java
@@ -0,0 +1,24 @@
+package com.rookiefly.dict.mysqldict.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MysqlDictServiceTest {
+
+    @Autowired
+    private MysqlDictService mysqlDictService;
+
+    @Test
+    public void testShowTables() {
+        List<String> tables = mysqlDictService.showTables();
+        Assert.assertEquals(5, tables.size());
+    }
+}
